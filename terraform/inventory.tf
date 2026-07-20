edb4b6ddb29972342f6bf9684b9dd607bc15d8dd
sigdown <vasilybichutsky@gmail.com>
2026-07-20 15:57:29 +0000

    feature(gateway): add SSH proxy jump

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -15,21 +15,26 @@ all:
             gateway-1:
               ansible_host: 192.168.1.100
               ansible_user: ubuntu
+              ansible_ssh_common_args: '-o ProxyJump=ubuntu@${beget_compute_instance.edge.ip_address}:2222'
         k3s_masters:
           hosts:
             k3s-master-1:
               ansible_host: 192.168.1.101
               ansible_user: ubuntu
+              ansible_ssh_common_args: '-o ProxyJump=ubuntu@${beget_compute_instance.edge.ip_address}:2222'
         k3s_workers:
           hosts:
             k3s-worker-1:
               ansible_host: 192.168.1.102
               ansible_user: ubuntu
+              ansible_ssh_common_args: '-o ProxyJump=ubuntu@${beget_compute_instance.edge.ip_address}:2222'
             k3s-worker-2:
               ansible_host: 192.168.1.103
               ansible_user: ubuntu
+              ansible_ssh_common_args: '-o ProxyJump=ubuntu@${beget_compute_instance.edge.ip_address}:2222'
             k3s-worker-3:
               ansible_host: 192.168.1.104
               ansible_user: ubuntu
+              ansible_ssh_common_args: '-o ProxyJump=ubuntu@${beget_compute_instance.edge.ip_address}:2222'
   EOT
 }
